d8c169d545410c75dc0686f0ff1b4a199ac15d27
Robert Khasanov <robert.khasanov@tu-dresden.de>
2016-11-06 18:46:24 +0100

    Generated mapping for exynos for dubinski_tiny testcase

--- mapper/patch_mapper.sh
+++ mapper/patch_mapper.sh
@@ -3,11 +3,11 @@
 # patch .ptt
 cd .ptt
 make clean 
-../../generator/pthreads/patch_pthreads.sh nbody.Pthreads.c 800
+../../generator/pthreads/patch_pthreads.sh nbody.Pthreads.c 8000
 make
 cd ..
 
 # patch .cpntrace
 cd .cpntrace
 CFILES=`find . -name '*.c'`
-for i in $CFILES; do ../../generator/pthreads/patch_pthreads.sh $i 800; done
+for i in $CFILES; do ../../generator/pthreads/patch_pthreads.sh $i 8000; done
